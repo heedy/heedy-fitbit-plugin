47d06aed88619b19bf672b5f48ead0f73c3852e1
Daniel R. Kumor <daniel@dkumor.com>
2019-11-26 03:53:18 -0500

    Updated for sources -> objects are exec -> run

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-from aiohttp import web
-
 from heedy import Plugin
+from aiohttp import web
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 p = Plugin()
 
 routes = web.RouteTableDef()
 
 
-@routes.get("/api/fitbit/hello")
+@routes.post("/app_create")
 async def index(request):
-    print("REQUEST", request)
-    return web.Response(text="Hello World!")
-
-@routes.get("/api/fitbit/fwd")
-async def index(request):
-    print("REQUEST", request)
-    return await p.respond_forwarded(request)
-
+    evt = await request.json()
+    print("REQUEST", request.headers, evt)
+    await p.notify({'app': evt["app"], 'key': "setup", "global": True,
+                    'title': "Link your fitbit account to heedy",
+                    'description': "Follow the instructions [here](https://google.com) to get a fitbit API key, and save it to the [app settings](https://google.com)"})
+    return web.Response(text="ok")
 
 app = web.Application()
 app.add_routes(routes)
 
-
+print("Running fitbit")
 # Runs the server over a unix domain socket. The socket is automatically placed in the data folder,
 # and not the plugin folder.
 web.run_app(app, path=f"{p.name}.sock")
