2982ac4dcb5fb86c3b766910a2280af6f9881cbf
Daniel R. Kumor <daniel@dkumor.com>
2019-12-13 04:10:41 -0500

    Preparing for updated python lib

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# heedy-fitbit-plugin
+# Heedy Fitbit Sync
 
-heedy plugin for gathering fitbit data
+This plugin syncs your fitbit data with heedy, so that you can easily access the raw data gathered by your wearable devices.
--- main.py
+++ main.py
@@ -8,15 +8,36 @@ p = Plugin()
 
 routes = web.RouteTableDef()
 
+server_url = p.config["config"]["url"]
+
 
 @routes.post("/app_create")
-async def index(request):
+async def app_create(request):
     evt = await request.json()
     print("REQUEST", request.headers, evt)
-    await p.notify({'app': evt["app"], 'key': "setup", "global": True,
-                    'title': "Link your fitbit account to heedy",
-                    'description': "Follow the instructions [here](https://google.com) to get a fitbit API key, and save it to the [app settings](https://google.com)"})
+    await p.notify("setup", "Link your fitbit account to heedy", app=evt["app"], _global=True,
+                   description=f"To give heedy access to detailed data, you need to register a new application with fitbit. The application must be of `personal` type, and must have the following callback URL: `{server_url}/api/fitbit/auth/{evt['app']}`\nAfter registering an application with fitbit, copy its details into your fitbit settings.",
+                   actions=[{"title": "Register App", "href": "https://dev.fitbit.com/apps/new", "new_window": True},
+                            {"title": "Settings", "icon": "fas fa-cog", "href": f"#/apps/{evt['app']}/settings"}],
+                   dismissible=False)
     return web.Response(text="ok")
+
+
+@routes.post("/app_settings_update")
+async def app_settings_update(request):
+    evt = await request.json()
+    print("REQUEST", request.headers, evt)
+    # Read the app, getting the necessary settings
+    await p.notify("setup", "Setting up...", app=evt["app"], description="", actions=[])
+    return web.Response(text="ok")
+
+
+@routes.get("/api/fitbit/auth/{app}")
+async def auth_callback(request):
+    appid = request.match_info['app']
+    print(request.rel_url.query)
+    print(request.headers)
+    raise web.HTTPFound(location=f"{server_url}/#/apps/{appid}")
 
 app = web.Application()
 app.add_routes(routes)
