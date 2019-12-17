9af232645b67456055cb5dc45b3637e9e8db2c3f
Daniel Kumor <daniel@dkumor.com>
2019-12-16 19:09:01 -0500

    Oauth2 authorization flow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from heedy import Plugin
-from aiohttp import web
+from aiohttp import web, BasicAuth
 import logging
+import urllib.parse
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -10,13 +11,14 @@ routes = web.RouteTableDef()
 
 server_url = p.config["config"]["url"]
 
+redirector = lambda x : f"{server_url}/api/fitbit/auth/{x}"
 
 @routes.post("/app_create")
 async def app_create(request):
     evt = await request.json()
     print("REQUEST", request.headers, evt)
     await p.notify("setup", "Link your fitbit account to heedy", app=evt["app"], _global=True,
-                   description=f"To give heedy access to detailed data, you need to register a new application with fitbit. The application must be of `personal` type, and must have the following callback URL: `{server_url}/api/fitbit/auth/{evt['app']}`\nAfter registering an application with fitbit, copy its details into your fitbit settings.",
+                   description=f"To give heedy access to detailed data, you need to register a new application with fitbit. The application must be of `personal` type, and must have the following callback URL: `{redirector(evt['app'])}`\nAfter registering an application with fitbit, copy its details into your fitbit settings.",
                    actions=[{"title": "Register App", "href": "https://dev.fitbit.com/apps/new", "new_window": True},
                             {"title": "Settings", "icon": "fas fa-cog", "href": f"#/apps/{evt['app']}/settings"}],
                    dismissible=False)
@@ -27,16 +29,37 @@ async def app_create(request):
 async def app_settings_update(request):
     evt = await request.json()
     print("REQUEST", request.headers, evt)
+    a = await p.apps[evt["app"]]
     # Read the app, getting the necessary settings
-    await p.notify("setup", "Setting up...", app=evt["app"], description="", actions=[])
+    # await a.notify("setup", "Setting up...", description="", actions=[])
+    settings = await a.settings
+    await a.notify("setup", "Authorize Access", 
+        description="Now that heedy has fitbit app credentials, you need to give the app access to your data.", 
+        actions=[{"title": "Authorize","href": settings["authorization_uri"] + "?" +urllib.parse.urlencode({
+            "response_type": "code",
+            "client_id": settings["client_id"],
+            "redirect_uri": redirector(evt["app"]),
+            "scope": "activity heartrate location nutrition profile settings sleep social weight"
+        })}])
     return web.Response(text="ok")
 
 
 @routes.get("/api/fitbit/auth/{app}")
 async def auth_callback(request):
     appid = request.match_info['app']
-    print(request.rel_url.query)
-    print(request.headers)
+    code = request.rel_url.query['code']
+    print("CODE",code)
+    a = await p.apps[appid]
+    settings = await a.settings
+    response = await p.session.s.post(settings["refresh_uri"],data={
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": redirector(appid)
+    },auth=BasicAuth(settings["client_id"],settings["client_secret"]))
+    resjson = await response.json()
+    await a.update(meta = resjson)
+    await a.notify("setup","Synchronizing...",description="Heedy is syncing your historical fitbit data. This message will disappear once all of your data is downloaded.",actions=[])
+    # print(request.headers)
     raise web.HTTPFound(location=f"{server_url}/#/apps/{appid}")
 
 app = web.Application()
